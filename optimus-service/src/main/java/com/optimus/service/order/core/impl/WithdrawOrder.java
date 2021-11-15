f64a6ae46435ea8a8d26e37af748cca8220a9fa7
hongp <271414609@qq.com>
2021-11-16 00:34:27 +0800

    手续费

--- optimus-service/src/main/java/com/optimus/service/order/core/impl/WithdrawOrder.java
+++ optimus-service/src/main/java/com/optimus/service/order/core/impl/WithdrawOrder.java
@@ -10,6 +10,7 @@ import com.optimus.service.order.core.BaseOrder;
 import com.optimus.service.order.dto.CreateOrderDTO;
 import com.optimus.service.order.dto.OrderInfoDTO;
 import com.optimus.service.order.dto.PayOrderDTO;
+import com.optimus.util.AssertUtil;
 import com.optimus.util.constants.RespCodeEnum;
 import com.optimus.util.constants.account.AccountTypeEnum;
 import com.optimus.util.constants.member.MemberCollectFeeWayEnum;
@@ -55,9 +56,14 @@ public class WithdrawOrder extends BaseOrder {
         MemberTransConfineDTO memberTransConfine = memberManager.getMemberTransConfineByMemberId(createOrder.getMemberId());
         BigDecimal fee = memberManager.getFee(createOrder.getOrderAmount(), memberTransConfine);
         orderInfo.setFee(fee);
+
+        AssertUtil.notEmpty(memberTransConfine.getCollectFeeWay(), RespCodeEnum.MEMBER_TRANS_PERMISSION_ERROR, "手续费收取方式未配置");
+
         if(StringUtils.pathEquals(MemberCollectFeeWayEnum.COLLECT_FEE_WAY_A.getCode(), memberTransConfine.getCollectFeeWay())){
             orderInfo.setActualAmount(orderInfo.getOrderAmount());
-        } else if (StringUtils.pathEquals(MemberCollectFeeWayEnum.COLLECT_FEE_WAY_B.getCode(), memberTransConfine.getCollectFeeWay())){
+        }
+
+        if (StringUtils.pathEquals(MemberCollectFeeWayEnum.COLLECT_FEE_WAY_B.getCode(), memberTransConfine.getCollectFeeWay())){
             orderInfo.setActualAmount(orderInfo.getOrderAmount().add(fee));
         }
 
