412c2402427d3f54aca57850ae8a565a2d854014
hongp <271414609@qq.com>
2021-11-16 23:50:07 +0800

    提现支付

--- optimus-service/src/main/java/com/optimus/service/order/core/impl/WithdrawOrder.java
+++ optimus-service/src/main/java/com/optimus/service/order/core/impl/WithdrawOrder.java
@@ -1,9 +1,12 @@
 package com.optimus.service.order.core.impl;
 
+import com.optimus.manager.account.AccountManager;
+import com.optimus.manager.account.dto.DoTransDTO;
 import com.optimus.manager.member.MemberManager;
 import com.optimus.manager.member.dto.MemberTransConfineDTO;
 import com.optimus.service.account.AccountService;
 import com.optimus.service.account.dto.AccountInfoDTO;
+import com.optimus.service.member.dto.MemberInfoDTO;
 import com.optimus.service.order.convert.OrderServiceConvert;
 import com.optimus.service.order.core.BaseOrder;
 import com.optimus.service.order.dto.CreateOrderDTO;
@@ -11,14 +14,18 @@ import com.optimus.service.order.dto.OrderInfoDTO;
 import com.optimus.service.order.dto.PayOrderDTO;
 import com.optimus.util.AssertUtil;
 import com.optimus.util.constants.RespCodeEnum;
+import com.optimus.util.constants.account.AccountChangeTypeEnum;
 import com.optimus.util.constants.account.AccountTypeEnum;
 import com.optimus.util.constants.member.MemberCollectFeeWayEnum;
+import com.optimus.util.constants.member.MemberTypeEnum;
 import com.optimus.util.exception.OptimusException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 提现
@@ -33,6 +40,9 @@ public class WithdrawOrder extends BaseOrder {
 
     @Autowired
     private MemberManager memberManager;
+
+    @Autowired
+    private AccountManager accountManager;
 
     /**
      * 创建订单
@@ -78,7 +88,38 @@ public class WithdrawOrder extends BaseOrder {
     @Override
     public void payOrder(PayOrderDTO payOrder) {
 
-        //
+        MemberInfoDTO superMemberInfo = payOrder.getSuperMemberInfo();
 
+        List<DoTransDTO> doTransList = new ArrayList<>();
+
+        // 自己减一笔余额
+        DoTransDTO selfTrans = new DoTransDTO();
+        selfTrans.setMemberId(payOrder.getMemberId());
+        selfTrans.setOrderId(payOrder.getOrderId());
+        selfTrans.setChangeType(AccountChangeTypeEnum.B_MINUS.getCode());
+        selfTrans.setAmount(payOrder.getActualAmount());
+        selfTrans.setRemark("提现");
+        doTransList.add(selfTrans);
+
+        // 平台加一笔
+        DoTransDTO sysTrans = new DoTransDTO();
+        sysTrans.setOrderId(payOrder.getOrderId());
+        sysTrans.setChangeType(AccountChangeTypeEnum.P_PLUS.getCode());
+        sysTrans.setAmount(payOrder.getFee());
+        sysTrans.setRemark("提现手续费");
+        doTransList.add(sysTrans);
+
+        // 如果上级是码商，则需要上级也需减一笔
+        if(StringUtils.pathEquals(MemberTypeEnum.MEMBER_TYPE_C.getCode(), superMemberInfo.getMemberType())){
+            DoTransDTO superTrans = new DoTransDTO();
+            superTrans.setMemberId(superMemberInfo.getMemberId());
+            superTrans.setOrderId(payOrder.getOrderId());
+            superTrans.setChangeType(AccountChangeTypeEnum.B_MINUS.getCode());
+            superTrans.setAmount(payOrder.getFee());
+            superTrans.setRemark("提现");
+            doTransList.add(superTrans);
+        }
+
+        accountManager.doTrans(doTransList);
     }
 }
