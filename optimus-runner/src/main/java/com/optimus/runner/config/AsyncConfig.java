efad4ac3f50a484ab0a3a39a85a17ebe1ad360a5
iccxp <742912156@qq.com>
2021-11-18 15:31:05 +0800

    异步线程配置

--- optimus-runner/src/main/java/com/optimus/runner/config/AsyncConfig.java
+++ optimus-runner/src/main/java/com/optimus/runner/config/AsyncConfig.java
@@ -1,5 +1,7 @@
 package com.optimus.runner.config;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
@@ -15,20 +17,27 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class AsyncConfig {
 
-    private static final int MAX_POOL_SIZE = 100;
+    private static final String THREAD_NAME_PREFIX = "async-task-";
 
-    private static final int CORE_POOL_SIZE = 50;
+    private static final int MAX_POOL_SIZE = 20;
 
-    private static final String THREAD_NAME_PREFIX = "async-task-thread-pool-";
+    private static final int CORE_POOL_SIZE = 5;
+
+    private static final int QUEUE_CAPACITY = 10;
+
+    private static final int KEEP_ALIVE_SECONDS = 100;
 
     @Bean("asyncTaskExecutor")
     public AsyncTaskExecutor asyncTaskExecutor() {
 
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
         executor.setMaxPoolSize(MAX_POOL_SIZE);
         executor.setCorePoolSize(CORE_POOL_SIZE);
-        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setKeepAliveSeconds(KEEP_ALIVE_SECONDS);
 
         executor.initialize();
 
