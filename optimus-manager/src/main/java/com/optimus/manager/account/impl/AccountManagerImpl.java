47f55aec3487959123a9cfc698df59f6378c8eea
iccxp <742912156@qq.com>
2021-11-21 11:51:06 +0800

    账户交易优化

--- optimus-manager/src/main/java/com/optimus/manager/account/impl/AccountManagerImpl.java
+++ optimus-manager/src/main/java/com/optimus/manager/account/impl/AccountManagerImpl.java
@@ -13,13 +13,15 @@ import com.optimus.manager.account.AccountManager;
 import com.optimus.manager.account.convert.AccountManagerConvert;
 import com.optimus.manager.account.dto.DoTransDTO;
 import com.optimus.manager.account.validate.AccountManagerValidate;
+import com.optimus.util.AssertUtil;
 import com.optimus.util.DateUtil;
+import com.optimus.util.constants.RespCodeEnum;
+import com.optimus.util.exception.OptimusException;
 
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -42,56 +44,104 @@ public class AccountManagerImpl implements AccountManager {
     @Override
     public boolean doTrans(List<DoTransDTO> doTransList) {
 
-        log.info("doTrans doTransList is {}", doTransList);
+        try {
 
-        // 账户交易参数验证
-        String validate = AccountManagerValidate.validateDoTrans(doTransList);
-        if (StringUtils.hasLength(validate)) {
-            log.info("doTrans validate is {}", validate);
+            log.info("doTrans doTransList is {}", doTransList);
+
+            // 账户交易参数验证
+            AccountManagerValidate.validateDoTrans(doTransList);
+
+            // 构造账户信息
+            List<AccountInfoDO> accountInfoList = buildAccountInfoList(doTransList);
+
+            // 更新账户
+            updateAccountInfoForTrans(accountInfoList);
+
+            // 批量新增账户日志
+            addBatchAccountLog(accountInfoList, doTransList);
+
+        } catch (Exception e) {
+            log.error("doTrans e is {}", e);
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
             return false;
         }
 
-        // 查询交易涉及账户
+        return true;
+
+    }
+
+    /**
+     * 构造账户信息
+     * 
+     * @param doTransList
+     * @return
+     */
+    private List<AccountInfoDO> buildAccountInfoList(List<DoTransDTO> doTransList) {
+
+        // 获得会员编号集合
         List<String> memberIdList = doTransList.stream().map(DoTransDTO::getMemberId).distinct().collect(Collectors.toList());
-        List<AccountInfoDO> accountInfoList = accountInfoDao.listAccountInfoByMemberIdLists(memberIdList);
-        if (CollectionUtils.isEmpty(accountInfoList)) {
-            log.info("doTrans accountInfoList is {}", accountInfoList);
-            return false;
-        }
 
-        // 实际涉及账户
+        // 查询会员所对应的所有账户信息
+        List<AccountInfoDO> accountInfoList = accountInfoDao.listAccountInfoByMemberIdLists(memberIdList);
+
+        // 断言:账户信息不为空
+        AssertUtil.notEmpty(accountInfoList, RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易查询账户信息为空");
+
+        // 获得所涉及的账户信息
         accountInfoList = AccountManagerConvert.getAccountInfoDOList(accountInfoList, doTransList);
-        if (CollectionUtils.isEmpty(accountInfoList)) {
-            log.info("doTrans getAccountInfoDOList is {}", accountInfoList);
-            return false;
-        }
-        log.info("doTrans getAccountInfoDOList is {}", accountInfoList);
+        log.info("buildAccountInfoList accountInfoList is {}", accountInfoList);
+
+        // 断言:涉及账户信息不为空
+        AssertUtil.notEmpty(accountInfoList, RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易构造账户信息异常");
+
+        return accountInfoList;
+
+    }
+
+    /**
+     * 更新账户信息
+     * 
+     * @param accountInfoList
+     */
+    private void updateAccountInfoForTrans(List<AccountInfoDO> accountInfoList) {
 
         // 更新账户
         int update = accountInfoDao.updateAccountInfoForTrans(accountInfoList, DateUtil.currentDate());
+        log.info("updateAccountInfoForTrans update is {}", update);
+
+        // 验证更新结果
         if (update != accountInfoList.size()) {
-            log.info("doTrans updateAccountInfoForTrans is {}, update is {}", accountInfoList.size(), update);
-            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
-            return false;
+            throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易更新账户异常");
         }
+
+    }
+
+    /**
+     * 批量新增账户日志
+     * 
+     * @param accountInfoDOList
+     * @param doTransList
+     */
+    private void addBatchAccountLog(List<AccountInfoDO> accountInfoDOList, List<DoTransDTO> doTransList) {
+
+        // 获得账户主键集合
+        List<Long> idList = accountInfoDOList.stream().map(AccountInfoDO::getId).collect(Collectors.toList());
 
         // 查询账户交易后的数据
-        List<Long> idList = accountInfoList.stream().map(AccountInfoDO::getId).collect(Collectors.toList());
-        accountInfoList = accountInfoDao.listAccountInfoByIdLists(idList);
-        log.info("doTrans listAccountInfoByIdLists is {}", accountInfoList);
+        List<AccountInfoDO> accountInfoList = accountInfoDao.listAccountInfoByIdLists(idList);
+        log.info("addBatchAccountLog accountInfoList is {}", accountInfoList);
+
+        // 获得账户日志
+        List<AccountLogDO> accountLogList = AccountManagerConvert.getAccountLogDOList(accountInfoList, doTransList);
+        log.info("addBatchAccountLog accountLogList is {}", accountLogList);
+
+        // 验证获得的账户日志
+        if (CollectionUtils.isEmpty(accountLogList)) {
+            throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易构造账户日志异常");
+        }
 
         // 记录账户日志
-        List<AccountLogDO> accountLogList = AccountManagerConvert.getAccountLogDOList(accountInfoList, doTransList);
-        if (CollectionUtils.isEmpty(accountLogList)) {
-            log.info("doTrans getAccountLogDOList is {}", accountLogList);
-            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
-            return false;
-        }
-        log.info("doTrans getAccountLogDOList is {}", accountLogList);
-
         accountLogDao.addBatchAccountLog(accountLogList);
-
-        return true;
 
     }
 
--- optimus-manager/src/main/java/com/optimus/manager/account/validate/AccountManagerValidate.java
+++ optimus-manager/src/main/java/com/optimus/manager/account/validate/AccountManagerValidate.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Set;
 
 import com.optimus.manager.account.dto.DoTransDTO;
+import com.optimus.util.constants.RespCodeEnum;
 import com.optimus.util.constants.account.AccountChangeTypeEnum;
 import com.optimus.util.constants.order.OrderTypeEnum;
+import com.optimus.util.exception.OptimusException;
 
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -23,16 +25,17 @@ public class AccountManagerValidate {
      * 验证账户交易入参
      * 
      * @param doTransList
-     * @return
      */
-    public static String validateDoTrans(List<DoTransDTO> doTransList) {
+    public static void validateDoTrans(List<DoTransDTO> doTransList) {
 
         if (CollectionUtils.isEmpty(doTransList)) {
-            return "账户交易入参不能为空";
+            throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易入参不能为空");
         }
 
         // 账户交易重复性
         Set<String> maSet = new HashSet<>();
+        Set<String> orderIdSet = new HashSet<>();
+        Set<String> orderTypeSet = new HashSet<>();
 
         for (DoTransDTO item : doTransList) {
 
@@ -45,47 +48,53 @@ public class AccountManagerValidate {
 
             // 非空验证
             if (!StringUtils.hasLength(memberId)) {
-                return "账户交易会员编号不能为空";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易会员编号不能为空");
             }
             if (!StringUtils.hasLength(orderId)) {
-                return "账户交易订单编号不能为空";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易订单编号不能为空");
             }
             if (!StringUtils.hasLength(orderType)) {
-                return "账户交易订单类型不能为空";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易订单类型不能为空");
             }
             if (!StringUtils.hasLength(changeType)) {
-                return "账户交易变更类型不能为空";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易变更类型不能为空");
             }
             if (amount == null) {
-                return "账户交易金额不能为空";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易金额不能为空");
             }
             if (!StringUtils.hasLength(remark)) {
-                return "账户交易备注不能为空";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易备注不能为空");
             }
 
             // 合法性验证
             if (OrderTypeEnum.instanceOf(orderType) == null) {
-                return "账户交易非法的订单类型";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易非法的订单类型");
             }
             if (AccountChangeTypeEnum.instanceOf(changeType) == null) {
-                return "账户交易非法的变更类型";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易非法的变更类型");
             }
             if (amount.compareTo(BigDecimal.ZERO) < 0) {
-                return "账户交易金额不能小于0";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易金额不能小于0");
             }
             if (amount.scale() > 4) {
-                return "账户交易金额精度为4";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易金额精度为4");
             }
 
             // 重复性验证
             String ma = memberId + changeType.substring(0, changeType.length() - 1);
             if (!maSet.add(ma)) {
-                return "账户交易同一账户不允许在同一事务中";
+                throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易同一账户不允许在同一事务中");
             }
+
+            orderIdSet.add(orderId);
+            orderTypeSet.add(orderType);
 
         }
 
-        return null;
+        if (orderIdSet.size() != 1 || orderTypeSet.size() != 1) {
+            throw new OptimusException(RespCodeEnum.ACCOUNT_TRANSACTION_ERROR, "账户交易订单号或订单类型不合法");
+        }
+
     }
 
 }
